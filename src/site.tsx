1b15f3e4f49f7713338f080d3daec1e9da65af30
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-09-04 15:48:08 +0200

    changes

--- src/site.tsx
+++ src/site.tsx
@@ -6,10 +6,10 @@ import {FloatingWhatsApp} from "react-floating-whatsapp"
 const Site = () => {
     return (
         <div>
-            <div className="bg-black d-flex justify-content-between p-2 text-white  align-items-center">
-                <div>
+            <div className="d-flex justify-content-between p-2 align-items-center">
+                <div className="d-flex flex-row align-items-center">
                     <img src="https://ngratesc.sirv.com/ristian/logo.png" width={60} className="img-fluid" />
-                    <span className="fs-5 custom_font">Ristian Consultancy</span>
+                    <h5 className="fs-5 custom_font primary_text">Grimaco <span className="secondary_text">Incorporated</span></h5>
                 </div>
                 <div>
                     <a href="tel:+263771939493" target="_blank"><button className="btn text-white primary_button">Call Us</button></a>
@@ -17,37 +17,33 @@ const Site = () => {
             </div>
             <div className="d-flex  justify-content-center align-items-center" style={{ ...bg_img("https://ngratesc.sirv.com/ristian/home.jpg"), height: "90vh" }}>
                 <div className="text-center bg-light bg-opacity-50 p-5 rounded">
-                    <h1>Your Gateway to Global Opportunities</h1>
-                    <p className="f fst-italic">"Connecting Talent with Top Employers in the UK,Poland,Canada, Bulgaria, Lithuania, and Australia"</p>
-                    <a href="https://wa.me/263771939493" target="_blank"><button className="btn primary_button">Find Out More</button></a>
+                    <h1>Grimaco Incorporated</h1>
+                    <p className="f fst-italic">"Your Satisfaction, Our Pleasure"</p>
+                    <a href="https://wa.me/263776034923" target="_blank"><button className="btn primary_button">Find Out More</button></a>
                 </div>
             </div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm  primary_background text-white p-5">
-                        <h1>Welcome to Ristian Consultancy</h1>
-                        <p>At Ristian Consultancy, we specialize in connecting talented professionals with leading companies across the UK, Poland, Canada, Bulgaria, Lithuania, and Australia. With over 100 partner companies, we offer a wide range of job placements tailored to fit your skills, educational background, and career aspirations.</p>
-                        <a href="https://wa.me/263771939493" target="_blank"><button className="btn secondary_button">Get In Touch</button></a>
+                    <div className="col-sm p-5">
+                        <h1 className="primary_text">Welcome to Grimaco Incorporated</h1>
+                        <p>At Grimaco Incorporated, we are dedicated to empowering businesses through expert consultancy, sales coaching, and comprehensive training solutions. Our commitment is to provide the highest quality services that meet the evolving needs of our clients. With a foundation built on integrity, excellence, and unwavering dedication, we strive to help your business not only survive but thrive in today’s competitive landscape. Welcome to a partnership where your success is our ultimate goal.</p>
+                        <a href="https://wa.me/263776034923" target="_blank"><button className="btn primary_button">Get In Touch</button></a>
                     </div>
                     <div className="col-sm  p-5" style={{ ...bg_img("https://ngratesc.sirv.com/ristian/home_second.jpg"), minHeight: "50vh" }}>
-
                     </div>
                 </div>
             </div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm bg-black order-md-2 text-white p-5 align-items-center d-flex">
+                    <div className="col-sm order-md-2 p-5 align-items-center d-flex">
                         <div>
-                            <h1>Our Expertise</h1>
-                            <p>We are dedicated to finding the perfect match between candidates and employers. Whether you're an experienced professional or just starting your career, our extensive network spans various industries, ensuring that we have the right opportunity for you.</p>
-                            <a href="https://wa.me/263771939493" target="_blank"><button className="btn primary_button">Get In Touch</button></a>
+                            <h1 className="secondary_text">Our Expertise</h1>
+                            <p>At Grimaco Incorporated, our expertise lies in transforming businesses through strategic consultancy, tailored sales coaching, and cutting-edge training programs. With years of experience across diverse industries, we bring a deep understanding of market dynamics, customer behaviors, and competitive challenges. Our team of seasoned professionals is adept at identifying opportunities for growth, optimizing sales strategies, and delivering impactful training that equips your workforce with the skills they need to excel. Whether you're looking to enhance operational efficiency, boost sales performance, or upskill your team, our expert guidance ensures you achieve your business objectives with precision and confidence.</p>
+                            <a href="https://wa.me/263776034923" target="_blank"><button className="btn secondary_button">Get In Touch</button></a>
                         </div>
-
                     </div>
-                    <div className="col-sm p-5" style={{ ...bg_img("https://ngratesc.sirv.com/ristian/home_third.jpg"), minHeight: "50vh" }}>
-
+                    <div className="col-sm primary_background p-5" style={{ ...bg_img("https://ngratesc.sirv.com/ristian/home_third.jpg"), minHeight: "50vh" }}>
                     </div>
-
                 </div>
             </div>
             <div className="container-fluid mt-5 mb-5">
@@ -55,21 +51,21 @@ const Site = () => {
                     <h1 className="text-black display-1 fw-bold text-center">Why Us?</h1>
                 </div>
                 <div className="row gap-2 m-4">
-                    <div className="col-sm text-center primary_background rounded text-white p-3">
+                    <div className="col-sm text-center secondary_background rounded text-white p-3">
                         <p className="fw-bold">Global Reach</p>
                         <p>Our connections span across four major countries, giving you access to international job markets.</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-globe" viewBox="0 0 16 16">
                             <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z" />
                         </svg>
                     </div>
-                    <div className="col-sm text-center primary_background rounded text-white p-3">
+                    <div className="col-sm text-center secondary_background rounded text-white p-3">
                         <p className="fw-bold">Tailored Placements</p>
                         <p>We consider your CV, educational background, and age to find the best possible job match for you.</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-hand-index" viewBox="0 0 16 16">
                             <path d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 1 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 0 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.035a.5.5 0 0 1-.416-.223l-1.433-2.15a1.5 1.5 0 0 1-.243-.666l-.345-3.105a.5.5 0 0 1 .399-.546L5 8.11V9a.5.5 0 0 0 1 0V1.75A.75.75 0 0 1 6.75 1M8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v5.34l-1.2.24a1.5 1.5 0 0 0-1.196 1.636l.345 3.106a2.5 2.5 0 0 0 .405 1.11l1.433 2.15A1.5 1.5 0 0 0 6.035 16h6.385a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5 5 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.6 2.6 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046zm2.094 2.025" />
                         </svg>
                     </div>
-                    <div className="col-sm text-center primary_background rounded text-white p-3">
+                    <div className="col-sm text-center secondary_background rounded text-white p-3">
                         <p className="fw-bold">Comprehensive Support</p>
                         <p>From the application process to job placement, we provide full support to ensure your success.</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-headset" viewBox="0 0 16 16">
@@ -78,34 +74,33 @@ const Site = () => {
                     </div>
                 </div>
             </div>
-            <div className="container-fluid primary_background text-white">
+            <div className="container-fluid">
                 <div>
-                    <h1 className="fw-bold display-2 text-center pt-5 pb-5">Our Services</h1>
+                    <h1 className="fw-bold display-2 text-center primary_text pt-5 pb-5">Our Services</h1>
                 </div>
                 <div className=" pb-4">
 
-                <div className="row gap-3 m-4 text-center">
-                    <div className="col-sm bg-black p-3 rounded">
+                <div className="row gap-3 m-4 text-center text-white">
+                    <div className="col-sm secondary_background p-3 rounded">
                         <p className="fw-bold">CV and Cover Letter Writing</p>
                         <p>Your CV and cover letter are often your first impression with potential employers. Our professional writing service ensures that your application materials stand out. We tailor each CV and cover letter to highlight your strengths, skills, and experiences, presenting you as the ideal candidate for the job. With our expert touch, you’ll confidently present your qualifications and make a lasting impression.</p>
                     </div>
-                    <div className="col-sm bg-black p-3 rounded">
+                    <div className="col-sm secondary_background p-3 rounded">
                         <p className="fw-bold">Training and Coaching</p>
                         <p>Success in your career requires more than just securing a job; it involves continuous learning and development. Our training and coaching services are designed to enhance your skills and prepare you for the demands of your new role. Whether you need industry-specific training, interview coaching, or guidance on career development, our experts provide personalized support to help you thrive in your career.</p>
                     </div>
-                    <div className="col-sm bg-black p-3 rounded">
+                    <div className="col-sm secondary_background p-3 rounded">
                         <p className="fw-bold">Employment Procurement</p>
                         <p>We specialize in matching talented individuals with top companies across the UK, Poland, Canada, Bulgaria, Lithuania, and Australia. Our recruitment service is personalized to ensure that we find the right fit for both the candidate and the employer. We work with over 100 companies in various industries, offering opportunities in healthcare, IT, education, hospitality, construction, and more. Whatever your field, we have the connections and expertise to help you find your next role.</p>
                     </div>
                 </div>
-                </div>
-
             </div>
-            <div className="text-center bg-black pt-5 pb-5">
-                <h1 className="fw-bold display-2 text-white">
+            </div>
+            <div className="text-center pt-5 pb-5">
+                <h1 className="fw-bold display-2">
                     In Demand Job Opportunities
                 </h1>
-                <p className="text-white">Our recruitment services cover a broad spectrum of professions, including</p>
+                <p className="">Our recruitment services cover a broad spectrum of professions, including</p>
                 <Marquee>
                     <div className="d-flex align-items-center p-2" style={{ minWidth: "250px" }}>
                         <div className="primary_background rounded-start p-2 ">
@@ -157,7 +152,6 @@ const Site = () => {
                         <div className="bg-light rounded-end p-2 w-100">
                             <span>Sales & Marketing</span>
                         </div>
-
                     </div>
                     <div className="d-flex align-items-center p-2" style={{ minWidth: "250px" }}>
                         <div className="primary_background rounded-start p-2">
@@ -171,9 +165,9 @@ const Site = () => {
                     </div>
                     <div className="d-flex align-items-center p-2" style={{ minWidth: "250px" }}>
                         <div className="primary_background rounded-start p-2">
-                           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-hearts text-white" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566M9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4m13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"/>
-</svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-hearts text-white" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566M9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4m13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"/>
+                            </svg>
                         </div>
                         <div className="bg-light rounded-end p-2 w-100">
                             <span>Hospitality & Domestic Services</span>
@@ -194,14 +188,11 @@ const Site = () => {
                     <div className="col-sm bg-white rounded d-flex  justify-content-center mb-2">
                             <div>
                             <br/>
-
                             <Star />
                             <br/>
-
                             <p className="fst-italic">Ristian Consultancy made my dream of working abroad a reality. Their team was incredibly supportive throughout the entire process, and I landed a fantastic job in the UK. Highly recommend their services!"</p>                    
                             <p className="fw-bold">Thandiwe Ndlovu</p>
                             </div>
-
                     </div>
                     <div className="col-sm bg-white rounded mb-2">
                     <br/>
@@ -228,13 +219,13 @@ const Site = () => {
                     <br />
                     Send an email to <a href="mailto:info@ristian.co.zw">info@ristian.co.zw</a>
                     <br/>
-                    Call on <a href="tel:+263771939493">+263 77 193 9493</a>
+                    Call on <a href="tel:+263776034923">+263 77 603 4923</a>
                 </p>
             </div>
             <div className="text-center primary_background ">
                 <span className="text-white">Made By <a className="text-white" href="https://aurorasystems.co.zw">Aurora</a></span>
             </div>
-        <FloatingWhatsApp phoneNumber="+263771939493" accountName="Ristian Consultancy" avatar="https://ngratesc.sirv.com/ristian/logo.png"/>
+        <FloatingWhatsApp phoneNumber="+263776034923" accountName="Ristian Consultancy" avatar="https://ngratesc.sirv.com/ristian/logo.png"/>
         </div>
     )
 }
